4625901645fed4881cf9fea1f264f41ba9cca336
noviyashik50285 <mynewdisk50285@gmail.com>
2024-11-11 09:10:02 +0300

    secmus2 commit

---
+++ backgr.js
@@ -0,0 +1,87 @@
+const button0 = document.getElementById('butn0');
+button0.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel.jpg)';
+});
+const button1 = document.getElementById('butn1');
+button1.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel0.jpg)';
+});
+const button2 = document.getElementById('butn2');
+button2.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel1.jpg)';
+});
+const button3 = document.getElementById('butn3');
+button3.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel2.jpg)';
+});
+const button4 = document.getElementById('butn4');
+button4.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel3.jpg)';
+});
+const button5 = document.getElementById('butn5');
+button5.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel4.jpg)';
+});
+const button6 = document.getElementById('butn6');
+button6.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel5.jpg)';
+});
+const button7 = document.getElementById('butn7');
+button7.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel6.jpg)';
+});
+const button8 = document.getElementById('butn8');
+button8.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel7.jpg)';
+});
+const button9 = document.getElementById('butn9');
+button9.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel8.jpg)';
+});
+const button10 = document.getElementById('butn10');
+button10.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel9.jpg)';
+});
+
+const button11 = document.getElementById('butn11');
+button11.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel10.jpg)';
+});
+const button12 = document.getElementById('butn12');
+button12.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel11.jpg)';
+});
+const button13 = document.getElementById('butn13');
+button13.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel12.jpg)';
+});
+const button14 = document.getElementById('butn14');
+button14.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel13.jpg)';
+});
+const button15 = document.getElementById('butn15');
+button15.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel14.jpg)';
+});
+const button16 = document.getElementById('butn16');
+button16.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/angel15.jpg)';
+});
+
